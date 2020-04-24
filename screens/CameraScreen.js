5d896ce723c645b91a3b81c1151474c492f6f19f
david-ron <ronci.david@gmail.com>
2020-04-24 18:58:01 -0400

    added dict to show our tests translated words

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,24 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Dimensions, Alert, ActivityIndicator, TouchableHighlight, Button } from 'react-native';
+import {
+    Text,
+    View,
+    StyleSheet,
+    Dimensions,
+    Alert,
+    ActivityIndicator,
+    TouchableHighlight,
+    Button,
+} from 'react-native';
 import { Camera } from 'expo-camera';
+
+const wordDict = {
+    wood: 'lemn',
+    paper: 'hârtie',
+    spoon: 'linguriţă',
+    cup: 'ceașcă',
+    bottle: 'sticla',
+    bowl: 'castron',
+    glasses: 'ochelari',
+    keys: 'chei',
+    scissors: 'foarfece',
+    ring: 'inel',
+    plate: 'farfurie',
+    'metal key': 'chei',
+    shears: 'foarfece',
+    flatware: 'farfurie',
+    leather: 'piele',
+};
 
 // Initialize Clarifai api
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
     //Note: ask for API key to test the app
-    apiKey: ''
+    apiKey: '',
 });
 process.nextTick = setImmediate;
 
-export default function CameraScreen ({ navigation }, props) {
+export default function CameraScreen({ navigation }, props) {
     navigation.setOptions({ headerShown: true, headerTitle: '' });
 
     const [hasPermission, setHasPermission] = useState(null);
     const [type, setType] = useState(Camera.Constants.Type.back);
     const [loading, setLoading] = React.useState(false);
     const [identifiedAs, setIdentifiedAs] = useState('');
-
 
     /**
      * Asynchronous method that takes a picture when the user presses the capture button
@@ -34,7 +60,7 @@ export default function CameraScreen ({ navigation }, props) {
 
             // Set options
             const options = {
-                base64: true
+                base64: true,
             };
 
             let photo = await camera.takePictureAsync(options);
@@ -47,20 +73,23 @@ export default function CameraScreen ({ navigation }, props) {
     /**
      * This method takes in an image in base64 form and sends it to the Clarifai API
      * who then predicts what the image contains, and then sends the results to the displayResult() methods
-     * @param {*} imageData 
+     * @param {*} imageData
      */
     const identifyImage = (imageData) => {
         // Identify the image
-        app.models.predict(Clarifai.GENERAL_MODEL, { base64: imageData })
-            .then((response) => displayResult(response.outputs[0].data.concepts[0].name)
-                .catch((err) => alert(err))
-            );
-    }
+        app.models
+            .predict(Clarifai.GENERAL_MODEL, { base64: imageData })
+            .then((response) => {
+                displayResult(
+                    response.outputs[0].data.concepts[0].name
+                ).catch((err) => alert(err));
+            });
+    };
 
     /**
      * This method displays the result fetched from the clarifai API and displays it
      * on the camera screen in the form of an alert
-     * @param {*} identifiedImage 
+     * @param {*} identifiedImage
      */
     const displayResult = (identifiedImage) => {
         // TODO: Fix this
@@ -69,14 +98,20 @@ export default function CameraScreen ({ navigation }, props) {
         // console.log(name);
 
         setLoading(false);
-
+        if (name.toLowerCase() in wordDict)
+            Alert.alert(
+                name + ' is ' + wordDict[name.toLowerCase()] + ' in romanian',
+                '',
+                {
+                    cancelable: false,
+                }
+            );
         // Show an alert with the answer
-        Alert.alert(
-            name, '', { cancelable: false })
+        else Alert.alert(name, '', { cancelable: false });
 
         // Resume the preview
         camera.resumePreview();
-    }
+    };
 
     useEffect(() => {
         (async () => {
@@ -92,12 +127,23 @@ export default function CameraScreen ({ navigation }, props) {
         return <Text>No access to camera</Text>;
     }
     return (
-        <Camera style={{ flex: 1 }} type={type} ref={ref => (camera = ref)}>
-            <ActivityIndicator size="large" style={styles.loadingIndicator} color="#fff" animating={loading} />
+        <Camera style={{ flex: 1 }} type={type} ref={(ref) => (camera = ref)}>
+            <ActivityIndicator
+                size="large"
+                style={styles.loadingIndicator}
+                color="#fff"
+                animating={loading}
+            />
             <TouchableHighlight style={styles.captureButton} disabled={loading}>
-                <Button color={'white'} onPress={capturePhoto} disabled={loading} title="" accessibilityLabel="Learn more about this button" />
+                <Button
+                    color={'white'}
+                    onPress={capturePhoto}
+                    disabled={loading}
+                    title=""
+                    accessibilityLabel="Learn more about this button"
+                />
             </TouchableHighlight>
-        </Camera >
+        </Camera>
     );
 }
 
@@ -119,8 +165,8 @@ const styles = StyleSheet.create({
         width: 80,
         height: 80,
         borderRadius: 50,
-        backgroundColor: "#1eb2a6",
-        justifyContent: "center",
-        left: "40%"
-    }
+        backgroundColor: '#1eb2a6',
+        justifyContent: 'center',
+        left: '40%',
+    },
 });
